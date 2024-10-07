5fad4940549984aec64b693dcd3b67316507e12f
vitorhaoliveira <vitorhaoliveira123@gmail.com>
2024-10-07 20:29:50 -0300

    ajustes seção explore

--- components/Explore.jsx
+++ components/Explore.jsx
@@ -2,12 +2,23 @@ import Image from "next/image";
 
 function Explore() {
   return (
-    <section className="py-12 ">
-      <div className="container mx-auto">
+    <section className="py-12 xl:py-0 xl:h-[90vh] xl:w-screen">
+      <div className="container mx-auto xl:w-full xl:h-full flex xl:justify-center xl:items-center">
         <div className="bg-yellow-200 w-full flex flex-col lg:flex-row gap-12 xl:gap-20 ">
-          <div>1</div>
-          <div>1</div>
-          <div>1</div>
+          <div className="flex-1 bg-blue-200">1</div>
+          <div className="hidden xl:flex justify-center">
+            <div className="relative w-[322px] h-[580px]">
+              <Image
+                src="/assets/explore/cup.png"
+                fill
+                alt=""
+                className="object-cover"
+                quality={100}
+                priority
+              />
+            </div>
+          </div>
+          <div className="flex-1 bg-pink-200">1</div>
         </div>
       </div>
     </section>
