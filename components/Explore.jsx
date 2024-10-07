52c63e9452bad5fb2c45f767a2f399942fda749c
vitorhaoliveira <vitorhaoliveira123@gmail.com>
2024-10-07 20:57:58 -0300

    ajustes seção explore

--- components/Explore.jsx
+++ components/Explore.jsx
@@ -5,7 +5,10 @@ function Explore() {
     <section className="py-12 xl:py-0 xl:h-[90vh] xl:w-screen">
       <div className="container mx-auto xl:w-full xl:h-full flex xl:justify-center xl:items-center">
         <div className="bg-yellow-200 w-full flex flex-col lg:flex-row gap-12 xl:gap-20 ">
-          <div className="flex-1 bg-blue-200">1</div>
+          <div className="flex-1 bg-blue-200 flex flex-col justify-around items-end text-center xl:text-left gap-12 xl:gap-0 max-w-[400px] mx-auto xl:max-w-none xl:mx-0">
+            <div>item 1</div>
+            <div>item 2</div>
+          </div>
           <div className="hidden xl:flex justify-center">
             <div className="relative w-[322px] h-[580px]">
               <Image
@@ -18,7 +21,10 @@ function Explore() {
               />
             </div>
           </div>
-          <div className="flex-1 bg-pink-200">1</div>
+          <div className="flex-1 bg-pink-200 flex flex-col justify-around items-end text-center xl:text-left gap-12 xl:gap-0 max-w-[400px] mx-auto xl:max-w-none xl:mx-0">
+            <div>item 3</div>
+            <div>item 4</div>
+          </div>
         </div>
       </div>
     </section>
