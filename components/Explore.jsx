0a9764f7d3eccf790fec104b0b88309abbe4488c
vitorhaoliveira <vitorhaoliveira123@gmail.com>
2024-10-08 21:07:17 -0300

    ajustes explore

--- components/Explore.jsx
+++ components/Explore.jsx
@@ -6,19 +6,51 @@ function Explore() {
       <div className="container mx-auto xl:w-full xl:h-full flex xl:justify-center xl:items-center">
         <div className="bg-yellow-200 w-full flex flex-col lg:flex-row gap-12 xl:gap-20 ">
           <div className="flex-1 bg-blue-200 flex flex-col justify-around items-end text-center xl:text-left gap-12 xl:gap-0 max-w-[400px] mx-auto xl:max-w-none xl:mx-0">
-            <div className="relative flex items-center">
+            <div className="relative flex items-start">
               <div className="xl:max-w-[420px] xl:text-right xl:flex xl:flex-col xl:items-end">
                 <div className="mb-6 flex justify-center items-center">
-                  <Image src="/assets/explore/coffees/coffee-1.svg" width={56} height={56} alt="" />
+                  <Image
+                    src="/assets/explore/coffees/coffee-1.svg"
+                    width={56}
+                    height={56}
+                    alt=""
+                  />
                 </div>
+                <h3 className="text-3xl xl:text-4xl font-primary font-bold mb-4">
+                  Misturas ricas de café expresso
+                </h3>
+                <p className="max-w-[400px]">
+                  Delicie-se com os sabores profundos e robustos das nossas
+                  misturas de café expresso habilmente elaboradas. Perfeito para
+                  um lanche rápido ou um deleite descontraído à tarde.
+                </p>
               </div>
             </div>
-            <div>item 2</div>
+            <div className="relative flex items-start">
+              <div className="xl:max-w-[420px] xl:text-right xl:flex xl:flex-col xl:items-end">
+                <div className="mb-6 flex justify-center items-center">
+                  <Image
+                    src="/assets/explore/coffees/coffee-1.svg"
+                    width={56}
+                    height={56}
+                    alt=""
+                  />
+                </div>
+                <h3 className="text-3xl xl:text-4xl font-primary font-bold mb-4">
+                  Misturas ricas de café expresso
+                </h3>
+                <p className="max-w-[400px]">
+                  Delicie-se com os sabores profundos e robustos das nossas
+                  misturas de café expresso habilmente elaboradas. Perfeito para
+                  um lanche rápido ou um deleite descontraído à tarde.
+                </p>
+              </div>
+            </div>
           </div>
           <div className="hidden xl:flex justify-center">
             <div className="relative w-[322px] h-[580px]">
               <Image
-                src="/assets/explore/cup.png" 
+                src="/assets/explore/cup.png"
                 fill
                 alt=""
                 className="object-cover"
@@ -26,11 +58,48 @@ function Explore() {
                 priority
               />
             </div>
-            <h3>Misturas ricas de café expresso</h3>
           </div>
-          <div className="flex-1 bg-pink-200 flex flex-col justify-around items-end text-center xl:text-left gap-12 xl:gap-0 max-w-[400px] mx-auto xl:max-w-none xl:mx-0">
-            <div>item 3</div>
-            <div>item 4</div>
+          <div className="flex-1 bg-pink-200 flex flex-col justify-around text-center xl:text-left gap-12 xl:gap-0 max-w-[400px] mx-auto xl:max-w-none xl:mx-0">
+          <div className="relative flex items-start">
+              <div className="xl:max-w-[420px] xl:text-left xl:flex xl:flex-col xl:items-start">
+                <div className="mb-6 flex justify-center items-center">
+                  <Image
+                    src="/assets/explore/coffees/coffee-1.svg"
+                    width={56}
+                    height={56}
+                    alt=""
+                  />
+                </div>
+                <h3 className="text-3xl xl:text-4xl font-primary font-bold mb-4">
+                  Misturas ricas de café expresso
+                </h3>
+                <p className="max-w-[400px]">
+                  Delicie-se com os sabores profundos e robustos das nossas
+                  misturas de café expresso habilmente elaboradas. Perfeito para
+                  um lanche rápido ou um deleite descontraído à tarde.
+                </p>
+              </div>
+            </div>
+            <div className="relative flex items-start">
+              <div className="xl:max-w-[420px] xl:text-left xl:flex xl:flex-col xl:items-start">
+                <div className="mb-6 flex justify-center items-center">
+                  <Image
+                    src="/assets/explore/coffees/coffee-1.svg"
+                    width={56}
+                    height={56}
+                    alt=""
+                  />
+                </div>
+                <h3 className="text-3xl xl:text-4xl font-primary font-bold mb-4">
+                  Misturas ricas de café expresso
+                </h3>
+                <p className="max-w-[400px]">
+                  Delicie-se com os sabores profundos e robustos das nossas
+                  misturas de café expresso habilmente elaboradas. Perfeito para
+                  um lanche rápido ou um deleite descontraído à tarde.
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
