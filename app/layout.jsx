85bf388b4562221cc993769c2d6501f262c42ee4
vitorhaoliveira <vitorhaoliveira123@gmail.com>
2024-10-21 17:57:34 -0300

    inicio footer

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import { Cormorant_Upright, Open_Sans } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 const cormorant_upright = Cormorant_Upright({
   subsets: ["latin"],
@@ -25,6 +26,7 @@ export default function RootLayout({ children }) {
       <body className={`${cormorant_upright.variable} ${open_sans.variable}`}>
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
--- components/OpeningHours.jsx
+++ components/OpeningHours.jsx
@@ -6,6 +6,7 @@ function OpeningHours() {
   return (
     <section className="flex bg-primary h-[60vh] xl:h-[80vh]">
       <div className="hidden relative xl:flex flex-1 justify-center items-center ">
+        <div className="w-full h-full absolute z-40 top-0 bg-black/60"></div>
         <Image
           src="/assets/opening-hours/img.png"
           fill
@@ -17,7 +18,23 @@ function OpeningHours() {
         <Badge containerStyles="w-[320px] h-[320px] absolute z-40" />
       </div>
 
-      <div></div>
+      <div className="flex-1 bg-opening_hours bg-cover bg-no-repeat flex flex-col justify-center items-center relative">
+        <div className="w-full h-full absolute z-10 top-0 bg-black/[0.85]"></div>
+        <div className="z-20 flex flex-col items-center justify-center">
+          <h2 className="h2 text-white mb-4">Horários</h2>
+          <Separator />
+          <div className="mt-12 relative w-[300px] h-[220px] xl:w-[470px] xl:h-[280px]">
+            <Image
+              src="/assets/opening-hours/program-badge.svg"
+              fill
+              className="object-contain"
+              quality={100}
+              priority
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
